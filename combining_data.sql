a84e6a2cc57ee2014eaf804accd459d0ed372a56
Ben Holmes <103968613+bennyboy230602@users.noreply.github.com>
2024-11-04 05:05:37 +0000

    Update combining_data.sql

--- combining_data.sql
+++ combining_data.sql
@@ -1,4 +1,5 @@
--- Convert "started_at" and "ended_at" attributes to TIMESTAMP datatype for the tables that generated the schema as STRING (2022_08 - 2022_10 and 2023_05 - 2023_07) while combining all tables
+-- Convert "started_at" and "ended_at" attributes to TIMESTAMP datatype as the generated the schema used STRING.
+-- Some files needed to be split to make the file size under 100MB to allow local file uploading.
 
 CREATE TABLE IF NOT EXISTS `divvy_trips.divvy_tripdata_combined` AS (
   SELECT ride_id,
@@ -14,7 +15,7 @@ CREATE TABLE IF NOT EXISTS `divvy_trips.divvy_tripdata_combined` AS (
   end_lat,
   end_lng,
   member_casual
-  FROM `divvy_trips.divvy_tripdata_2022_08`
+  FROM `divvy_trips.2022-09`
 
   UNION ALL
   
@@ -31,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `divvy_trips.divvy_tripdata_combined` AS (
   end_lat,
   end_lng,
   member_casual
-  FROM `divvy_trips.divvy_tripdata_2022_09`
+  FROM `divvy_trips.2022-09-2`
 
   UNION ALL
 
@@ -48,37 +49,7 @@ CREATE TABLE IF NOT EXISTS `divvy_trips.divvy_tripdata_combined` AS (
   end_lat,
   end_lng,
   member_casual
-  FROM `divvy_trips.divvy_tripdata_2022_10`
-
-  UNION ALL
-
-  SELECT * 
-  FROM `divvy_trips.divvy_tripdata_2022_11`
-
-  UNION ALL
-
-  SELECT * 
-  FROM `divvy_trips.divvy_tripdata_2022_12`
-
-  UNION ALL
-
-  SELECT * 
-  FROM `divvy_trips.divvy_tripdata_2023_01`
-
-  UNION ALL
-
-  SELECT * 
-  FROM `divvy_trips.divvy_tripdata_2023_02`
-
-  UNION ALL
-
-  SELECT * 
-  FROM `divvy_trips.divvy_tripdata_2023_03`
-
-  UNION ALL
-
-  SELECT * 
-  FROM `divvy_trips.divvy_tripdata_2023_04`
+  FROM `divvy_trips.2022-10`
 
   UNION ALL
 
@@ -95,7 +66,109 @@ CREATE TABLE IF NOT EXISTS `divvy_trips.divvy_tripdata_combined` AS (
   end_lat,
   end_lng,
   member_casual
-  FROM `divvy_trips.divvy_tripdata_2023_05`
+  FROM `divvy_trips.2022-11`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2022-12`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-01`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-02`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-03`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-04`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-05`
 
   UNION ALL
 
@@ -112,7 +185,7 @@ CREATE TABLE IF NOT EXISTS `divvy_trips.divvy_tripdata_combined` AS (
   end_lat,
   end_lng,
   member_casual 
-  FROM `divvy_trips.divvy_tripdata_2023_06`
+  FROM `divvy_trips.2023-06`
 
   UNION ALL
 
@@ -129,5 +202,73 @@ CREATE TABLE IF NOT EXISTS `divvy_trips.divvy_tripdata_combined` AS (
   end_lat,
   end_lng,
   member_casual
-  FROM `divvy_trips.divvy_tripdata_2023_07`
+  FROM `divvy_trips.2023-06-2`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-07`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-07-2`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-08`
+
+  UNION ALL
+
+  SELECT ride_id,
+  rideable_type,
+  CAST(started_at AS TIMESTAMP) as start_time,
+  CAST(ended_at AS TIMESTAMP) as end_time,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  member_casual
+  FROM `divvy_trips.2023-08-2`
 )
